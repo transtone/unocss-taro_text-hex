87161fde92a87f05a0e7012559a246eeca4cccdd
transtone <zm3345@126.com>
2022-04-21 15:01:11 +0800

    fix: fix color

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
   <view class="index-home p-20px">
     <view class="text-center">
       <text class="text-red">这是一段红色文字</text>
-      <view class="text-16px text-hex-3336699">
+      <view class="text-16px text-hex-336699">
         测试文字颜色
       </view>
       <view class="pt-10px I_text-hex-336699">这段文字用了加强颜色</view>
